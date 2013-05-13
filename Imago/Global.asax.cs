fa7973ffa25103aeae562d973b56fe46bc941e82
Jon Rozier <jon.rozier@gmail.com>
2013-05-13 18:04:59 -0500

    Created Global.asax
    Set up start/stop log entries

---
+++ Imago/Global.asax.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Imago.Utilities;
+
+namespace Imago
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            // Set up Log4Net
+            log4net.Config.XmlConfigurator.Configure();
+            Logger.Info("Application Started...");
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+            Logger.Info("Application Ending...");
+        }
+    }
+}
