b6c0e3ff09939568e52f4016e8746807706c854f
Jon Rozier <jon.rozier@gmail.com>
2013-05-13 17:52:58 -0500

    Constants file

---
+++ Imago/Bean/Constants.cs
@@ -0,0 +1,16 @@
+﻿namespace Imago.Bean
+{
+    public class Constants
+    {
+        public const string BackgroundDirectoryParseKey = "BackgroundDirectoryParse";
+
+        public const string ApplicationKey_MediaUploadParsing = "IsParsingMediaUploadDirectory";
+        public const string ApplicationKey_MediaItems = "MediaItems";
+
+        public const string MediaObjectBackupLocation = "/App_Data/Media/";
+        public const string SettingsKey_DatabasePath = "DatabasePath";
+        public const string SettingsKey_MediaDirectory = "MediaDirectory";
+        public const string SettingsKey_MediaUploadDirectory = "MediaUploadDirectory";
+        public const string SettingsFileLocation = "/Resources/settings.config";
+    }
+}
